d5c35c9e13568851a4261d4aa3b56ad2b77f6b36
smile-1006 <singhswapnil060303@gmail.com>
2026-07-13 19:48:27 +0530

    feat: update appBaseUrl to point to the new Material Management environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   appName: 'EAS MM',
   version: '1.0.0',
-  appBaseUrl: 'https://bmoc-angular.vercel.app',
+  appBaseUrl: 'https://mm-material-management.vercel.app',
 };
 
 
